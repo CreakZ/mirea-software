82cbc171c66a54c0bd8d7a3fba8a0779963a044e
Maxim <kaktotak587@gmail.com>
2024-11-19 22:37:25 +0300

    removed duplicated work-7 directory

--- pr-7/docker_py/server/server.py
+++
@@ -1,8 +0,0 @@
-import http.server
-import socketserver
-
-# Эта переменная нужна для обработки запросов клиента к серверу.
-handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", 1234), handler) as httpd:
-    httpd.serve_forever()
